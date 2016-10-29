655f0e41d2b3e82aedd4ad92a4dda7b91f699f8f
Simple-Shyam <shyam055119@gmail.com>
2016-10-29 18:44:38 +0530

    compilation

---
+++ shyammacro/HFcodes/run.C
@@ -0,0 +1,5 @@
+{
+gSystem->SetIncludePath("-I. -I$ALICE_ROOT/include -I$ALICE_PHYSICS/include -I$ALICE_ROOT/ANALYSIS/macros -I$ROOTSYS/include");
+gROOT->LoadMacro("AliDhCorrelationExtractionoff.cxx++g");
+gROOT->ProcessLine(".x ExtractOutput_Dplusoff.C");
+}
